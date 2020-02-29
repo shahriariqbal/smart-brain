a75776834f6986d42a13531a094d349db6163f12
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-01 00:45:58 +0600

    Rank Component & Particles Background Added
    particles.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+particles: {
+
+  number: {
+    value: 200,
+    density: {
+      enable: true,
+      value_area: 800
+    }
+  }
+
+
+
+
+                  // line_linked: {
+                  //   shadow: {
+                  //     enable: true,
+                  //     color: "#3CA9D1",
+                  //     blur: 5
+                  //   }
+                  // }
+
+
+
+                }
+
+}
 
 function App() {
   return (
     <div className="App">
 
+
+    <Particles className='particles'
+                params={particlesOptions} 
+                />
+
+
+
+
     <Navigation/>
     <Logo/>
+    <Rank/>
     <ImageLinkForm/>
+    
     {
  
   
